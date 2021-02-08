b5a63df3b6125bf0180d4387f4d68a4ed4fcd9b3
tianlin-pc <troylin0218@163.com>
2021-02-08 13:03:50 +0800

    修复人物百科页面无法'去除'秒懂百科的问题

--- CloseBaiduSecondUnderstandEncyclopedia/CloseBaiduSecondUnderstandEncyclopedia.js
+++ CloseBaiduSecondUnderstandEncyclopedia/CloseBaiduSecondUnderstandEncyclopedia.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name                关闭百度百科中的‘秒懂百科’视频以及右侧的‘V百科’
 // @namespace           https://greasyfork.org/
-// @version             1.0.0
+// @version             1.1.0
 // @description         关闭百度百科页面中的‘秒懂百科’视频以及右侧的‘V百科’;使用比较low的方式,带佬莫笑我 -.-;感谢AC的大力支持@AC
 // @author              XingTianyu
 // @match               *://baike.baidu.com/item/*
 // @icon                https://baike.baidu.com/favicon.ico
 // @grant               none
+// @note                2021.02.08-V1.1.0 使用AC教我的给元素添加'display:none'属性的方式来实现视觉'去除'，修复人物百科页面无法'去除'的问题
 // @note                2021.01.31-V1.0.0 简单粗暴的删除页面元素
 // @license             MIT
 // ==/UserScript==
@@ -15,7 +16,7 @@
     'use strict';
 
     /**
-     * 老方法，获取待删除节点的父节点，然后用父节点删除目标节点
+     * 老方法，获取待删除节点的父节点，然后用父节点删除目标节点 已废除
      * @param {待删除元素的class名称}
      */
     function removeElementByHimFather(className) {
@@ -35,7 +36,10 @@
      */
     var hideElement = function (className) {
         var hideEle = document.querySelector('.' + className);
-        hideEle.style.cssText = "display:none";
+        //先判断是否存在再继续操作
+        if (hideEle) {
+            hideEle.style.cssText = "display:none";
+        }
     }
 
 
@@ -54,7 +58,7 @@
 
 
     /**
-     * 使用定时器删除
+     * 使用定时器删除 已废除
      * @param {待删除的元素class名称数组} classNameArray 
      */
     function timerToRemove(classNameArray) {
@@ -85,8 +89,10 @@
         console.log("%c[星田雨-“关闭百度百科中的'秒懂百科'视频以及右侧的'V百科'等”] %c感谢使用，不相关的元素已隐藏～", "font-weight:bold;color:darkorange", "color:0");
     }
 
-    //秒懂百科、v百科、xx、yy、'下载APP'、分享按钮、'TA说'右侧小卡片、'TA说'下面的div
-    var classNameArray = ['before-content', 'lemmaWgt-promotion-vbaike', 'lemmaWgt-promotion-slide', 'topA', 'appdownload', 'share-list', 'tashuo-right', 'tashuo-bottom'];
+
+    //************************************************************************************* */
+    //秒懂百科、v百科、xx、yy、'下载APP'、分享按钮、'TA说'右侧小卡片、'TA说'下面的div、人物百科的秒懂百科视频所在div的className
+    var classNameArray = ['before-content', 'lemmaWgt-promotion-vbaike', 'lemmaWgt-promotion-slide', 'topA', 'appdownload', 'share-list', 'tashuo-right', 'tashuo-bottom', 'poster-middle'];
     // timerToRemove(classNameArray);
     safeWaitFunc(classNameArray, removeByLoopArray);
 
